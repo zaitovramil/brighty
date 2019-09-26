9b0f6b37efff0e8a308463f3e0395b9bbb2ba880
zaitovramil <39480077+zaitovramil@users.noreply.github.com>
2019-09-26 15:16:00 +0600

    backbutton event update

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		document.addEventListener("backbutton", navigator.app.exitApp(), false);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
